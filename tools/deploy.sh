43ede86b1d345767af3ece826e307c9e009da34c
kif11 <kovalewskiy@gmail.com>
2020-10-12 12:25:59 -0400

    Deploy package.json. Ignore .vscode

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -9,5 +9,14 @@ rsync -av server.mjs kiko@codercat.tk:~/codercat2/server.mjs
 # Sync projec list
 rsync -av constants/projects.json kiko@codercat.tk:~/codercat2/constants/projects.json
 
+# Sync package.json to install server dependencies
+rsync -av package.json kiko@codercat.tk:~/codercat2/package.json
+
+# Install dependencies (One time only)
+# NOTE: Do it manually e.g.
+# ssh kiko@codercat.tk
+# cd codercat2
+# yarn
+
 # Restart the server
 ssh -t kiko@codercat.tk sudo systemctl restart codercat
