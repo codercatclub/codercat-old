62d8e1aee0165fafa6e1ea5e2a18bb22a297008b
kif11 <kovalewskiy@gmail.com>
2020-10-12 12:06:25 -0400

    Added projects to sync

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 # Sync static codercat build
-rsync -av out/ kiko@codercat.tk:/home/kiko/codercat2/out/
+rsync -av out/ kiko@codercat.tk:~/codercat2/out/
 
 # Sync the server
-rsync -av server.mjs kiko@codercat.tk:/home/kiko/codercat2/server.mjs
+rsync -av server.mjs kiko@codercat.tk:~/codercat2/server.mjs
+
+# Sync projec list
+rsync -av constants/projects.json kiko@codercat.tk:~/codercat2/constants/projects.json
 
 # Restart the server
 ssh -t kiko@codercat.tk sudo systemctl restart codercat
