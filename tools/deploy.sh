0884f2948321586b2b2ed1f97ae9606733558407
kif11 <kovalewskiy@gmail.com>
2021-07-21 19:10:39 -0400

    Updated deploy script. Fixed spell error

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -5,21 +5,21 @@ export CODERCAT_SERVER_URL="https://codercat.tk"
 yarn build
 
 # Sync static codercat build
-rsync -av out/ kiko@codercat.tk:~/codercat2/out/
+rsync -av out/ kiko@codercat.tk:~/codercat/out/
 
 # Sync the server
-rsync -av server.mjs kiko@codercat.tk:~/codercat2/server.mjs
+rsync -av server.mjs kiko@codercat.tk:~/codercat/server.mjs
 
 # Sync projec list
-rsync -av constants/projects.json kiko@codercat.tk:~/codercat2/constants/projects.json
+rsync -av constants/projects.json kiko@codercat.tk:~/codercat/constants/projects.json
 
 # Sync package.json to install server dependencies
-rsync -av package.json kiko@codercat.tk:~/codercat2/package.json
+rsync -av package.json kiko@codercat.tk:~/codercat/package.json
 
 # Install dependencies (One time only)
 # NOTE: Do it manually e.g.
 # ssh kiko@codercat.tk
-# cd codercat2
+# cd codercat
 # yarn
 
 # Restart the server
